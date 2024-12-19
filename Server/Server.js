1ba38a34c5fd732090352789a564f07df9e84564
Elange Franklin <114111024+Elangefranklin@users.noreply.github.com>
2024-12-19 13:36:47 +0100

    ok

--- Server/Server.js
+++ Server/Server.js
@@ -1,3 +1,3 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
